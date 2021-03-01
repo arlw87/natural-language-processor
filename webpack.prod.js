befabf56a2c5baf88cdf6481a44b3e1c18ea658b
Andrew White <arlw87@googlemail.com>
2021-03-01 13:20:35 +0000

    css output in production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -12,7 +13,7 @@ module.exports = {
         rules:[
             {
                 test: /\.scss$/,
-                use:['style-loader', 'css-loader', 'sass-loader']
+                use:[ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
         ]
     }, 
@@ -20,6 +21,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: path.resolve(__dirname, "src", "client", "views", "index.html"),
             filename: "./index.html"
-        })
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'})
     ]
 }
