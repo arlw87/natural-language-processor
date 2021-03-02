d920e0dc4ab03d4e00e32779544c3e3287eb648a
arlw87 <arlw87@googlemail.com>
2021-03-02 19:08:06 +0000

    started to style the webpage

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,9 @@
-import './styles/style.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/results.scss'
+import './styles/main.scss'
+import './styles/header.scss'
 import './js/formHandler'
 
 console.log('Hi');
