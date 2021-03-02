66aa629cb33d17679c7280e51208d80972f11cdf
Andrew White <arlw87@googlemail.com>
2021-03-02 09:41:54 +0000

    add api key from meaning cloud to server code

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
@@ -7,7 +9,11 @@ app.use(bodyParser.json());
 
 app.use(express.static('dist'));
 
+//API KEY
+const api_key = process.env.API_KEY;
+
 const port = 8080;
 const server = app.listen(port, ()=>{
     console.log(`server running on port ${port}`);
+    console.log(api_key);
 });
